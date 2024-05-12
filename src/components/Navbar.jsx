b3fd0ce60e18758c680f4577d16394e721b5d7e1
Lynczera <leonardochen.code@gmail.com>
2024-05-11 22:22:27 -0600

    implement burger for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,64 @@
-import { Anchor, Avatar, Flex, Group } from "@mantine/core";
+import {
+  Anchor,
+  Avatar,
+  Burger,
+  Flex,
+  Group,
+  Transition,
+  Paper,
+  Container,
+} from "@mantine/core";
 import classes from "./Navbar.module.css";
 import { useState } from "react";
-import {useNavigate, useLocation} from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
 export function Navbar() {
   const links = [
     { link: "/", label: "Leonardo Chen" },
     { link: "/Projects", label: "Projects" },
     { link: "/CV", label: "CV" },
   ];
-
+  const [opened, { toggle, close }] = useDisclosure(false);
   const nav = useNavigate();
   const location = useLocation();
   const [active, setActive] = useState(location.pathname);
-  const items = links.map((link) => (
-    link.label !=="CV"?
-    (
-    <Anchor
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      underline="never"
-      mt={8}
-      p={5}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-        nav(link.link);
-      }}
-    >
-      {link.label}
-    </Anchor>
-    ):
-    <Anchor
-      key={link.label}
-      href={"Leonardo_Chen_Resume.pdf"}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      underline="never"
-      mt={8}
-      p={5}
-      onClick={(event) => {
+  const items = links.map((link) =>
+    link.label !== "CV" ? (
+      <Anchor
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        data-active={active === link.link || undefined}
+        underline="never"
+        mt={8}
+        p={5}
+        onClick={(event) => {
+          event.preventDefault();
+          setActive(link.link);
+          nav(link.link);
+        }}
+      >
+        {link.label}
+      </Anchor>
+    ) : (
+      <Anchor
+        key={link.label}
+        href={"Leonardo_Chen_Resume.pdf"}
+        className={classes.link}
+        data-active={active === link.link || undefined}
+        underline="never"
+        mt={8}
+        p={5}
+        onClick={() => {
           close();
-      }}
-    >
-      {link.label}
-    </Anchor>
-  ));
+        }}
+      >
+        {link.label}
+      </Anchor>
+    ),
+  );
   return (
+
     <Flex h={"65px"} className={classes.inner}>
       <Avatar
         mt={4}
@@ -57,9 +67,24 @@ export function Navbar() {
         size={"lg"}
         src="leonardodevicon.png"
       />
-      <Group w={"100%"} justify="flex-end" mr={"100px"} gap="lg">
+      <Group className={classes.links} w={"100%"} justify="flex-end" mr={"100px"} gap="lg">
         {items}
       </Group>
+      <Burger
+        opened={opened}
+        onClick={toggle}
+        className={classes.burger}
+        size="md"
+        hiddenFrom="sm"
+      />
+
+      <Transition transition="scale-y" duration={200} mounted={opened}>
+        {(styles) => (
+          <Paper className={classes.dropdown} withBorder style={{...styles, zIndex:10}}>
+            {items}
+          </Paper>
+        )}
+      </Transition>
     </Flex>
   );
 }
